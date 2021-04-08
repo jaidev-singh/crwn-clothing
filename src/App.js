83153a112dbf9941f20f54aacc5b6c016a92cfca
gravitchaudhary2013 <gravit.chaudhary2013@gmail.com>
2021-04-09 01:05:47 +0530

    added- shop page & collection-preview, collecton-item components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => <h1>Hats Page</h1>
+import ShopPage from './pages/shop/shoppage.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
